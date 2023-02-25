81535618895d0d5bdec21dfcd7d02796273834ec
keannu125 <noehbernasol0@gmail.com>
2023-02-25 21:41:13 +0800

    Move back to axios for api

--- src/Components/Api/Api.tsx
+++ src/Components/Api/Api.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
-import { get } from "http";
 
 export function GetNotes() {
-  return fetch("http://localhost:8000/api/v1/notes/").then((res) => {
-    console.log(res);
-    return res.json();
+  return axios.get("http://localhost:8000/api/v1/notes/").then((res) => {
+    return res.data;
   });
+  /*return fetch("http://localhost:8000/api/v1/notes/").then((res) => {
+    console.log(res.body);
+    return res.json();
+  });*/
 }
