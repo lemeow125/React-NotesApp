23a394643dfd97a437cc83d3a0c3c8feeaa3c3ba
keannu125 <noehbernasol0@gmail.com>
2023-03-25 13:00:36 +0800

    Move to hash router and change url accordingly

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import Home from "./Routes/Home/Home";
 import NewNote from "./Routes/NewNote/NewNote";
@@ -19,7 +23,7 @@ import Store from "./Features/Redux/Store/Store";
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Home />,
